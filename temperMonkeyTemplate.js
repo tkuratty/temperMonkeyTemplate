ee10d83d21b3ad8b83dab2bd1d3b6dea9ca722a2
tkuratty <47928719+tkuratty@users.noreply.github.com>
2020-02-11 08:00:42 +0900

    set templat

---
+++ temperMonkeyTemplate.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Your module Name
+// @namespace    your name space
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://*/*
+// @grant        none
+// ==/UserScript==
+
+//(function() {
+//    'use strict';
+//});
+
+window.onload = function() {
+    // write your code here
+}
